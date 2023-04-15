72b41b8e5028e4538d7e4774157b5053659400d3
vikas0077 <127411205+vikas0077@users.noreply.github.com>
2023-04-15 14:15:33 +0530

    Update backend.php

--- backend.php
+++ backend.php
@@ -1,24 +1,26 @@
 <?php
-$username = "root";
-$password = "";
-$server = "localhost";
-$db = "dataa";
+    $name = $_POST['name'];
+    $dob = $_POST['dob'];
+    $email = $_POST['email'];
+    $phonenumber = $_POST['phonenumber'];
+    $address = $_POST['address'];
+    $gender = $_POST['gender'];
 
-$con = mysqli_connect($server, $username, $password, $db);
-if ($con) {
-    echo "connection successful..";
-} else {
-    echo "Connection failed..";
-}
-mysqli_select_db($con, 'dataa');
-$name = $_POST['name'];
-$dob = $_POST['dob'];
-$email = $_POST['email'];
-$phonenumber = $_POST['phonenumber'];
-$address = $_POST['address'];
-$gender = $_POST['gender'];
-$query = "INSERT INTO userinfo(name, lastname, dob, email, phonenumber,address, gender) 
-          VALUES ('$name', '$dob', '$email', '$phonenumber', '$address', '$gender')";
-mysqli_query($con, $query);
+    // Database connection
+    $conn = new mysqli('localhost:3308','root','pggc11','test');
+    if($conn->connect_error){
+        echo "$conn->connect_error";
+        die("Connection Failed : ". $conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("INSERT INTO userinfo(name, dob, email, phonenumber, address, gender) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssii", $name, $dob, $email, $phonenumber, $address, $gender);
+        $execval = $stmt->execute();
+        if($execval) {
+            echo "Registration successful...";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+        $stmt->close();
+        $conn->close();
+    }
 ?>
- 
